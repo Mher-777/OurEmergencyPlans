5457f1f398f2a7135416410ce9377ccf814298cc
Mher-777 <mherjan2003@mail.ru>
2020-03-02 19:28:35 +0400

    added header and  main-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function () {
-    
+    $('.main-slider__wrapper').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        fade: true,
+        speed: 400,
+        rows: 0,
+        cssEase: 'linear',
+        pauseOnHover: false,
+    })
 })
