63d457db6b3ed94bcd5f4d44d0f9b3475b04660e
Mher-777 <mherjan2003@mail.ru>
2020-03-03 21:27:14 +0400

    added scroll top

--- app/js/main.js
+++ app/js/main.js
@@ -50,6 +50,25 @@ $(function () {
         $('body').removeClass('shadow');
         $('.header').removeClass('shadow');
     });
+    
+    $.fn.scrollToTop = function () {
+        $(this).hide().removeAttr("href");
+        if ($(window).scrollTop() != "0") {
+            $(this).fadeIn("slow")
+        }
+        var scrollDiv = $(this);
+        $(window).scroll(function () {
+            if ($(window).scrollTop() == "0") {
+                $(scrollDiv).fadeOut("slow")
+            } else {
+                $(scrollDiv).fadeIn("slow")
+            }
+        });
+        $(this).click(function () {
+            $("html, body").animate({ scrollTop: 0 }, "slow")
+        })
+    };
+    $(function () { $("#toTop").scrollToTop(); });
     new WOW().init({
         offset: 50, 
     });
