ee6cb6fc3697cb3c9a3a1a7dd21f3ea5732afeb9
Mher-777 <mherjan2003@mail.ru>
2020-03-03 14:41:31 +0400

    end desktop version

--- app/js/main.js
+++ app/js/main.js
@@ -10,11 +10,13 @@ $(function () {
         cssEase: 'linear',
         pauseOnHover: false,
     });
+
     $('.features__item-text').mCustomScrollbar({
         theme: "light-thin",
         setHeight: false,
         scrollButtons: false,
-    })
+    });
+
     $('.banner__slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -23,5 +25,19 @@ $(function () {
         rows: 0,
         speed: 500,
         cssEase: 'linear',
-    })
-})
+    });
+
+    $(".faq-page__box").not(":first").hide();
+    $(".faq-page__tabs .faq-page__item").click(function () {
+        $(".faq-page__tabs .faq-page__item").removeClass("active").eq($(this).index()).addClass("active");
+        $(".faq-page__box").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+    $('.faq__read-more').click(function () {
+        if ($(this).next().is(':hidden')) {
+            $(this).next().slideDown('slow');
+        } else {
+            $(this).next().slideUp('slow');
+        }
+    });
+
+});
